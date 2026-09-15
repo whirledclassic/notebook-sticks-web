c5ed899c2a822657787f0d6992e85d5ff4bda224
whirledclassic <josh.awe99@gmail.com>
2026-09-15 18:56:26 -0400

    Load playable client from the Notebook Sticks source commit

--- game.js
+++ game.js
@@ -1,2 +1,11 @@
-/* loaded next */
-console.log('placeholder');
+/* Play loop from whirledclassic/notebook-sticks public/game.js */
+(function () {
+  var s = document.createElement("script");
+  s.src = "https://cdn.jsdelivr.net/gh/whirledclassic/notebook-sticks@b8cb29f8a6af0aac40ce984355e5d7a659527b48/public/game.js";
+  s.onerror = function () {
+    var t = document.createElement("script");
+    t.src = "https://raw.githubusercontent.com/whirledclassic/notebook-sticks/b8cb29f8a6af0aac40ce984355e5d7a659527b48/public/game.js";
+    document.body.appendChild(t);
+  };
+  document.body.appendChild(s);
+})();
